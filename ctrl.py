392411124c2e71534311bfd4dade4b057474c707
mikk0git <kasprzakmilan1@gmail.com>
2023-01-24 21:00:24 +0100

    + keep track of "burned" XP

--- ctrl.py
+++ ctrl.py
@@ -1,8 +1,23 @@
 import time
 import functions
+import os
+from datetime import date
 
-onStartTime = time.time()
-print(onStartTime)
+today = date.today()
+print(today)
+
+if os.path.exists(f"onStartTime/onStartTime{today}.txt"):
+    print("The file exists.")
+    with open(f"onStartTime/onStartTime{today}.txt", "r") as f:
+        onStartTime = f.read()
+        onStartTime = float(onStartTime)
+else:
+    print("The file does not exist.")
+    onStartTime = time.time()
+    print(onStartTime)
+    onStartTimeStr = str(onStartTime)
+    with open(f"onStartTime/onStartTime{today}.txt", "w") as f:
+        f.write(onStartTimeStr)
 
 
 
@@ -11,7 +26,9 @@ while True:
     time.sleep(10)
     xp_today = functions.dailyXP()['xp_today']
     if time.time() - xp_today*120 > onStartTime:
+        
         print("Zamykanie")
+        
     print("ok")
 
 
